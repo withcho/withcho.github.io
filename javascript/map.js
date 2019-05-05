f637f263c5012ca0aa37d5eb4361767a8be89976
withcho <49378576+withcho@users.noreply.github.com>
2019-05-05 22:12:04 +0900

    Create map.js

---
+++ javascript/map.js
@@ -0,0 +1,59 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 35.689614, lng: 139.691585},
+    zoom: 3,
+    minZoom: 3,
+    restriction: {
+      latLngBounds: {north: 85, south: -85, west: -180, east: 180},
+      strictBounds: true
+    },
+    disableDefaultUI: true,
+    styles: [
+      {
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#ffefd7"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#A5DEE4"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#ffa4a4"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#ff8080"
+          }
+        ]
+      }
+    ]
+  });
+}
