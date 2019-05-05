f1f3d7edfdeba5ca1a8dcedde0d622ad0c52ca21
withcho <49378576+withcho@users.noreply.github.com>
2019-05-05 23:21:51 +0900

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,17 @@
 $(function(){
 
   // MENUを押すとhide-menuクラスが外れてポップアップメニューが展開する
-  $('#Menu').on('click', function(){
+  $('.Menu').on('click', function(){
     $('.menu-wrap').removeClass('hide-menu');
-    document.getElementById("Menu").innerHTML = "Close";
+    var elements = document.getElementsByClassName("Menu");
+    elements[0].innerHTML = "Close";
   });
 
   // CLOSEを押すとhide-menuクラスが付与されてポップアップメニューが閉じる
-  $('#Close').on('click', function(){
+  $('.Close').on('click', function(){
     $('.menu-wrap').addClass('hide-menu');
+    var elements = document.getElementsByClassName("Close");
+    elements[0].innerHTML = "Menu";
   });
 
 });
