665b6b636d38212dba3f0c6ca52ec7671ae3c2f6
withcho <49378576+withcho@users.noreply.github.com>
2019-05-06 00:19:14 +0900

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 $(function(){
   
   $('#Menu').on('click', function() {
-    $('#menu-wrap').slideToggle('slow');
+    $('#menu-wrap').slideToggle(500);
     if ($(this).text() === 'Menu') {
       $(this).text('Close');
     } else {
