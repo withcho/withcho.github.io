83d2e6f226dff1a2b4a4be5822e09a386ca05285
withcho <49378576+withcho@users.noreply.github.com>
2019-05-06 21:48:22 +0900

    Update map.js

--- javascript/map.js
+++ javascript/map.js
@@ -1,6 +1,4 @@
 var map;
-var marker;
-var infoWindow;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -60,16 +58,30 @@ function initMap() {
     ]
   });
   
-  marker = new google.maps.Marker({
-    position: {lat: 35.689614, lng: 139.691585},
-    map: map
-  });
+  //DirectionsService のオブジェクトを生成
+  var directionsService = new google.maps.DirectionsService();
   
-  infoWindow = new google.maps.InfoWindow({
-    content: '<div>東京</div>'
-  });
+  //DirectionsRenderer のオブジェクトを生成
+  var directionsRenderer = new google.maps.DirectionsRenderer();
   
-  marker.addListener('click', function() {
-    infoWindow.open(map, marker);
+  //directionsRenderer と地図を紐付け
+  directionsRenderer.setMap(map); 
+  
+  //リクエストの出発点の位置（Empire State Building 出発地点の緯度経度）
+  var start = new google.maps.LatLng(40.748541, -73.985758);  
+  //リクエストの終着点の位置（Grand Central Station 到着地点の緯度経度）
+  var end = new google.maps.LatLng( 40.752741,-73.9772);  
+  
+  // ルートを取得するリクエスト
+  var request = {
+    origin: start,      // 出発地点の緯度経度
+    destination: end,   // 到着地点の緯度経度
+    travelMode: 'WALKING' //トラベルモード（歩き）
+  };
+  
+  //DirectionsService のオブジェクトのメソッド route() にリクエストを渡し、
+  //コールバック関数で結果を setDirections(result) で directionsRenderer にセットして表示
+  directionsService.route(request, function(result, status) {
+    directionsRenderer.setDirections(result);
   });
 }
