68b173914aa35f0ea2c13deb5c75ab4131667fec
withcho <49378576+withcho@users.noreply.github.com>
2019-05-16 21:20:59 +0900

    Update map.js

--- javascript/map.js
+++ javascript/map.js
@@ -61,46 +61,64 @@ function initMap() {
 
 function calculateAndDisplayRoute(map) {
   // America Route
-  var start_us = new google.maps.LatLng(37.939978, -122.325833);
-  var end_us = new google.maps.LatLng(40.712050, -74.043890);
-  var waypoints_us = [
-    { location: new google.maps.LatLng(34.010350, -118.496184) },
-    { location: new google.maps.LatLng(33.008058, -116.955410) },
-    { location: new google.maps.LatLng(33.254149, -115.472654) },
-    { location: new google.maps.LatLng(36.138250, -115.096837) },
-    { location: new google.maps.LatLng(34.866481, -111.759951) },
-    { location: new google.maps.LatLng(36.059168, -112.109308) },
-    { location: new google.maps.LatLng(36.901534, -111.452688) },
-    { location: new google.maps.LatLng(37.006504, -110.214807) },
+  var points_us = [
+    new google.maps.LatLng(37.939978, -122.325833),
+    new google.maps.LatLng(34.010350, -118.496184),
+    new google.maps.LatLng(33.008058, -116.955410),
+    new google.maps.LatLng(33.254149, -115.472654),
+    new google.maps.LatLng(36.138250, -115.096837),
+    new google.maps.LatLng(34.866481, -111.759951),
+    new google.maps.LatLng(36.059168, -112.109308),
+    new google.maps.LatLng(36.901534, -111.452688),
+    new google.maps.LatLng(37.006504, -110.214807),
+    new google.maps.LatLng(40.712050, -74.043890)
   ];
-  var request_us = {
-    origin: start_us,
-    destination: end_us,
-    waypoints: waypoints_us,
-    travelMode: 'DRIVING'
-  };
-  new google.maps.DirectionsService().route(request_us, function(result, status) {
-    if (status == 'OK') {
-      console.log(result);
-      new google.maps.DirectionsRenderer({
-        map: map,
-        directions: result,
-        preserveViewport: true,
-        suppressMarkers: true,
-        polylineOptions: {
-          strokeOpacity: 0.35,
-          strokeColor: "black",
-          strokeWeight: 5
+  
+  var start_us = null, waypoints_us = [], end_us = null;
+  var request_us_index = 0;
+  
+  for (var i = 0; i < points_us.length; i++) {
+    makeMarker(points_us[i], map);
+    // 開始経緯度をセット
+    if (start_us == null) {
+      start_us = points_us[i];
+    }
+    // 終了経緯度をセット
+    else if (waypoints.length == 8 || i == points_us.length - 1) {
+      end_us = points_us[i];
+  
+      var request_us[request_us_index] = {
+        origin: start_us,
+        destination: end_us,
+        waypoints: waypoints_us,
+        travelMode: 'DRIVING'
+      };
+      new google.maps.DirectionsService().route(request_us[request_us_index], function(result, status) {
+        if (status == 'OK') {
+          new google.maps.DirectionsRenderer({
+            map: map,
+            directions: result,
+            preserveViewport: true,
+            suppressMarkers: true,
+            polylineOptions: {
+              strokeOpacity: 0.35,
+              strokeColor: "black",
+              strokeWeight: 5
+            }
+          });
+        } else {
+          console.log(status);
         }
       });
-      var icon = new google.maps.MarkerImage('/images/pin.png');
-      makeMarker(start_us, icon, "start_us", map);
-      makeMarker(end_us, icon, "end_us", map);
-      for (i=0; i<waypoints_us.length; i++) {
-        makeMarker(waypoints_us[i], icon, "waypoint", map);
-      }
+      request_us_index++;
+      start_us = points[i];
+      waypoints_us = [];
     }
-  });
+    // waypointsを追加
+    else {
+      waypoints_us.push({ location: points[i], stopover: true });
+    }
+  }
 
   // Australia Route
   var start2 = new google.maps.LatLng(39.904844, 116.401543);  
@@ -123,18 +141,16 @@ function calculateAndDisplayRoute(map) {
           strokeWeight: 5
         }
       });
-      var icon = new google.maps.MarkerImage('/images/pin.png');
-      makeMarker(start2, icon, "start2", map);
-      makeMarker(end2, icon, "end2", map);
+      makeMarker(start2, map);
+      makeMarker(end2, map);
     }
   });
 }
 
-function makeMarker(position, icon, title, map) {
+function makeMarker(position, map) {
   new google.maps.Marker({
     position: position,
     map: map,
-    icon: icon,
-    title: title
+    icon: new google.maps.MarkerImage('/images/pin.png'),
   });
 }
