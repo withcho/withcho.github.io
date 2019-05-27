292fb175aa43f5a2a7f6affd9f3c590b24de8945
withcho <49378576+withcho@users.noreply.github.com>
2019-05-28 00:41:36 +0900

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,41 @@
-$(function(){
+$(function() {
   
   $('#menu_item1').on('click', function() {
-    $('.menu_wrap').hide("slide", 1000);
-    $('#menu_wrap1').show("slide", 1000);
+    if($('#menu_wrap2').css('display') != 'none' || $('#menu_wrap3').css('display') != 'none' || $('#menu_wrap4').css('display') != 'none') {
+      $('.menu_wrap').hide("slide", 1000);
+    }
+    if($('#menu_wrap1').css('display') == 'none') {
+      $('#menu_wrap1').show("slide", 1000);
+    }
   });
   $('#menu_item2').on('click', function() {
-    $('.menu_wrap').hide("slide", 1000);
-    $('#menu_wrap2').show("slide", 1000);
+    if($('#menu_wrap1').css('display') != 'none' || $('#menu_wrap3').css('display') != 'none' || $('#menu_wrap4').css('display') != 'none') {
+      $('.menu_wrap').hide("slide", 1000);
+    }
+    if($('#menu_wrap2').css('display') == 'none') {
+      $('#menu_wrap2').show("slide", 1000);
+    }
   });
   $('#menu_item3').on('click', function() {
-    $('.menu_wrap').hide("slide", 1000);
-    $('#menu_wrap3').show("slide", 1000);
+    if($('#menu_wrap1').css('display') != 'none' || $('#menu_wrap2').css('display') != 'none' || $('#menu_wrap4').css('display') != 'none') {
+      $('.menu_wrap').hide("slide", 1000);
+    }
+    if($('#menu_wrap3').css('display') == 'none') {
+      $('#menu_wrap3').show("slide", 1000);
+    }
   });
   $('#menu_item4').on('click', function() {
-    $('.menu_wrap').hide("slide", 1000);
-    $('#menu_wrap4').show("slide", 1000);
+    if($('#menu_wrap1').css('display') != 'none' || $('#menu_wrap2').css('display') != 'none' || $('#menu_wrap3').css('display') != 'none') {
+      $('.menu_wrap').hide("slide", 1000);
+    }
+    if($('#menu_wrap4').css('display') == 'none') {
+      $('#menu_wrap4').show("slide", 1000);
+    }
   });
   $('#menu_item5').on('click', function() {
-    $('.menu_wrap').hide("slide", 1000);
+    if($('.menu_wrap').css('display') != 'none') {
+      $('.menu_wrap').hide("slide", 1000);
+    }
   });
 
 });
