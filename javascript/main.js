be5aa5ec38d8e77160322de4128211f8e04e936f
withcho <49378576+withcho@users.noreply.github.com>
2019-05-27 11:51:22 +0900

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,18 @@
 $(function(){
   
-  $('.menu_item').on('click', function() {
+  $('#menu_item1').on('click', function() {
+    $('#menu-wrap').toggle("slide", 1000);
+  });
+  $('#menu_item2').on('click', function() {
+    $('#menu-wrap').toggle("slide", 1000);
+  });
+  $('#menu_item3').on('click', function() {
+    $('#menu-wrap').toggle("slide", 1000);
+  });
+  $('#menu_item4').on('click', function() {
+    $('#menu-wrap').toggle("slide", 1000);
+  });
+  $('#menu_item5').on('click', function() {
     $('#menu-wrap').toggle("slide", 1000);
   });
 
