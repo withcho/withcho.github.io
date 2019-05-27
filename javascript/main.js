d81226d6ed2513b9ed31f926540d8e78075d3b61
withcho <49378576+withcho@users.noreply.github.com>
2019-05-28 00:47:11 +0900

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -2,39 +2,39 @@ $(function() {
   
   $('#menu_item1').on('click', function() {
     if($('#menu_wrap2').css('display') != 'none' || $('#menu_wrap3').css('display') != 'none' || $('#menu_wrap4').css('display') != 'none') {
-      $('.menu_wrap').hide("slide", 1000);
+      $('.menu_wrap').toggle("slide", 1000);
     }
     if($('#menu_wrap1').css('display') == 'none') {
-      $('#menu_wrap1').show("slide", 1000);
+      $('#menu_wrap1').toggle("slide", 1000);
     }
   });
   $('#menu_item2').on('click', function() {
     if($('#menu_wrap1').css('display') != 'none' || $('#menu_wrap3').css('display') != 'none' || $('#menu_wrap4').css('display') != 'none') {
-      $('.menu_wrap').hide("slide", 1000);
+      $('.menu_wrap').toggle("slide", 1000);
     }
     if($('#menu_wrap2').css('display') == 'none') {
-      $('#menu_wrap2').show("slide", 1000);
+      $('#menu_wrap2').toggle("slide", 1000);
     }
   });
   $('#menu_item3').on('click', function() {
     if($('#menu_wrap1').css('display') != 'none' || $('#menu_wrap2').css('display') != 'none' || $('#menu_wrap4').css('display') != 'none') {
-      $('.menu_wrap').hide("slide", 1000);
+      $('.menu_wrap').toggle("slide", 1000);
     }
     if($('#menu_wrap3').css('display') == 'none') {
-      $('#menu_wrap3').show("slide", 1000);
+      $('#menu_wrap3').toggle("slide", 1000);
     }
   });
   $('#menu_item4').on('click', function() {
     if($('#menu_wrap1').css('display') != 'none' || $('#menu_wrap2').css('display') != 'none' || $('#menu_wrap3').css('display') != 'none') {
-      $('.menu_wrap').hide("slide", 1000);
+      $('.menu_wrap').toggle("slide", 1000);
     }
     if($('#menu_wrap4').css('display') == 'none') {
-      $('#menu_wrap4').show("slide", 1000);
+      $('#menu_wrap4').toggle("slide", 1000);
     }
   });
   $('#menu_item5').on('click', function() {
     if($('.menu_wrap').css('display') != 'none') {
-      $('.menu_wrap').hide("slide", 1000);
+      $('.menu_wrap').toggle("slide", 1000);
     }
   });
 
