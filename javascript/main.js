79bb4490fb74508e4c1d4915168162c4098ded78
withcho <49378576+withcho@users.noreply.github.com>
2019-05-27 13:21:09 +0900

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,23 @@
 $(function(){
   
   $('#menu_item1').on('click', function() {
-    $('.menu_wrap').hide();
-    $('#menu_wrap1').toggle("slide", 1000);
+    $('.menu_wrap').hide("slide", 1000);
+    $('#menu_wrap1').show("slide", 1000);
   });
   $('#menu_item2').on('click', function() {
-    $('.menu_wrap').hide();
-    $('#menu_wrap2').toggle("slide", 1000);
+    $('.menu_wrap').hide("slide", 1000);
+    $('#menu_wrap2').show("slide", 1000);
   });
   $('#menu_item3').on('click', function() {
-    $('.menu_wrap').hide();
-    $('#menu_wrap3').toggle("slide", 1000);
+    $('.menu_wrap').hide("slide", 1000);
+    $('#menu_wrap3').show("slide", 1000);
   });
   $('#menu_item4').on('click', function() {
-    $('.menu_wrap').hide();
-    $('#menu_wrap4').toggle("slide", 1000);
+    $('.menu_wrap').hide("slide", 1000);
+    $('#menu_wrap4').show("slide", 1000);
   });
   $('#menu_item5').on('click', function() {
-    $('.menu_wrap').hide();
+    $('.menu_wrap').hide("slide", 1000);
   });
 
 });
