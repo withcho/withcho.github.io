7da053ff8b7b9b38133a7aec8c40a94c5c859140
withcho <49378576+withcho@users.noreply.github.com>
2019-05-27 10:22:58 +0900

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 $(function(){
   
-  $('#Menu').on('click', function() {
+  $('.menu_item').on('click', function() {
     $('#menu-wrap').toggle("slide", 1000);
     if ($(this).text() === 'Menu') {
       $(this).text('Close');
