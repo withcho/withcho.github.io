88f859d429993c6ba52cf2365cd93e4332d5e65c
withcho <49378576+withcho@users.noreply.github.com>
2019-05-28 22:21:00 +0900

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,40 +1,10 @@
 $(function() {
   
-  $('#menu_item1').on('click', function() {
-    if($('#menu_wrap2').css('display') != 'none' || $('#menu_wrap3').css('display') != 'none' || $('#menu_wrap4').css('display') != 'none') {
-      $('.menu_wrap').hide("slide", 1000);
-    }
-    if($('#menu_wrap1').css('display') == 'none') {
-      $('#menu_wrap1').show("slide", 1000);
-    }
-  });
-  $('#menu_item2').on('click', function() {
-    if($('#menu_wrap1').css('display') != 'none' || $('#menu_wrap3').css('display') != 'none' || $('#menu_wrap4').css('display') != 'none') {
-      $('.menu_wrap').hide("slide", 1000);
-    }
-    if($('#menu_wrap2').css('display') == 'none') {
-      $('#menu_wrap2').show("slide", 1000);
-    }
-  });
-  $('#menu_item3').on('click', function() {
-    if($('#menu_wrap1').css('display') != 'none' || $('#menu_wrap2').css('display') != 'none' || $('#menu_wrap4').css('display') != 'none') {
-      $('.menu_wrap').hide("slide", 1000);
-    }
-    if($('#menu_wrap3').css('display') == 'none') {
-      $('#menu_wrap3').show("slide", 1000);
-    }
-  });
-  $('#menu_item4').on('click', function() {
-    if($('#menu_wrap1').css('display') != 'none' || $('#menu_wrap2').css('display') != 'none' || $('#menu_wrap3').css('display') != 'none') {
-      $('.menu_wrap').hide("slide", 1000);
-    }
-    if($('#menu_wrap4').css('display') == 'none') {
-      $('#menu_wrap4').show("slide", 1000);
-    }
-  });
-  $('#menu_item5').on('click', function() {
-    if($('.menu_wrap').css('display') != 'none') {
-      $('.menu_wrap').hide("slide", 1000);
+  $('.menu_item').on('click', function() {
+    $('.menu_wrap').hide("slide", 1000);
+    var wrap_item = $(this).attr("id") + "_wrap";
+    if($("#" + wrap_item).css('display') == 'none') {
+      $("#" + wrap_item).show("slide", 1000);
     }
   });
 
