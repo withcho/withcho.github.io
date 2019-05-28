9f7b20d7f806fc08d621a7da5b12070ca3103d35
withcho <49378576+withcho@users.noreply.github.com>
2019-05-28 23:18:00 +0900

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,9 @@
 $(function() {
   
   $('.menu_item').on('click', function() {
-    $('.menu_wrap').fadeOut(1000);
     var wrap_item = $(this).attr("id") + "_wrap";
     if($("#" + wrap_item).css('display') == 'none') {
+      $('.menu_wrap').fadeOut(1000);
       $("#" + wrap_item).fadeIn(1000);
     }
   });
