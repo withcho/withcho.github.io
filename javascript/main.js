d39bc907e85622673dae729ade551892494c0dc4
withcho <49378576+withcho@users.noreply.github.com>
2019-06-14 19:24:46 +0900

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,28 @@
+// Loading page
 $(function() {
-  
-  // show menu
-  $('.menu_item').on('click', function() {
-    var wrap_item = $(this).attr("id") + "_wrap";
-    if($("#" + wrap_item).css('display') == 'none') {
-      $('.menu_wrap').fadeOut();
-      $("#" + wrap_item).fadeIn();
-    }
-  });
-  
-  $('#menu_item5').on('click', function() {
-    $('.menu_wrap').fadeOut();
-  });
-
+  var loading = function(){
+    $('#Loading').fadeOut();
+  }
+  setTimeout(loading, 6000);
 });
 
-var loading = function(){
-  $('#Loading').fadeOut();
-}
-setTimeout(loading, 6000);
+// show menu
+$('.menu_item').on('click', function() {
+  var wrap_item = $(this).attr("id") + "_wrap";
+  if($("#" + wrap_item).css('display') == 'none') {
+    $('.menu_wrap').fadeOut();
+    $("#" + wrap_item).fadeIn();
+  }
+});
+
+$('#menu_item5').on('click', function() {
+  $('.menu_wrap').fadeOut();
+});
+
+// show scroll
+$('#menu_item3_wrap').on('scroll', function() {
+  if($(this).scrollTop() != 0) {
+    $('.scroll_arrow').fadeOut();
+  } else {
+    $('.scroll_arrow').fadeIn();
+  }
